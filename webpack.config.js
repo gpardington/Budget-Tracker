c9e7f962edbd5c7dba023dc3340d0e53202fb30e
Grant Pardington <grant.pardington@gmail.com>
2020-11-01 13:27:04 -0700

    Added module, rules, and Babel to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,20 @@ const config = {
     filename: 'bundle.js'
   },
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
   plugins: [
     new WebpackPwaManifest({
         //Name of the generated manifest file
