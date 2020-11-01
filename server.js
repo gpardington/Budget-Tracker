d793958e7447f83aa054a6253375aa90a27ce97c
Grant Pardington <grant.pardington@gmail.com>
2020-11-01 12:41:45 -0700

    Added process.env for MongoDB server to server.js file

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+//Sets up the Express App
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+//Middleware
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -23,6 +24,7 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
+//Start App
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
